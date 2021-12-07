142d0b5ca21d5670163b97fbd995741f936556c7
Stefanos Michalas <sm@think-plus.gr>
2021-12-07 13:36:34 +0200

    sort player actors in generic helper function

---
+++ src/helpers.ts
@@ -0,0 +1,29 @@
+import { colorTable, combatant } from "./types";
+
+export const sortPlayerActors = (combatants: combatant[]) => {
+  return combatants.sort((a, b) =>
+    b.initiative - a.initiative === 0
+      ? b.alignment === "PARTY"
+        ? +1
+        : -1
+      : b.initiative - a.initiative
+  );
+};
+
+export const color = (
+  index: number,
+  turnNumber: number,
+  combatActor: combatant,
+  value: string,
+  isText?: boolean
+) => {
+  return index === turnNumber
+    ? combatActor.color && colorTable["Default"] !== combatActor.color
+      ? !!isText
+        ? combatActor.color.secondary
+        : combatActor.color.primary
+      : combatActor.alignment === "PARTY"
+      ? "success.main"
+      : "error.main"
+    : value;
+};
