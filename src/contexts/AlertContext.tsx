9f59770ffd71ddac58dd42af3951276797d1b0c5
Stefanos Michalas <sm@think-plus.gr>
2021-12-15 21:39:21 +0200

    added alert context for actor remove, set message for each alert

--- src/contexts/AlertContext.tsx
+++ src/contexts/AlertContext.tsx
@@ -6,7 +6,13 @@ type AlertContextProviderProps = {
 
 type AlertContextType = {
   actorSubmitAlertOpen: boolean;
+  actorSubmitAlertMessage: string;
   setActorSubmitAlertOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setActorSubmitAlertMessage: React.Dispatch<React.SetStateAction<string>>;
+  actorRemoveAlertOpen: boolean;
+  actorRemoveAlertMessage: string;
+  setActorRemoveAlertOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setActorRemoveAlertMessage: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const AlertContext = createContext<AlertContextType>(
@@ -16,10 +22,25 @@ export const AlertContext = createContext<AlertContextType>(
 const AlertContextProvider = ({ children }: AlertContextProviderProps) => {
   const [actorSubmitAlertOpen, setActorSubmitAlertOpen] =
     useState<boolean>(false);
+  const [actorSubmitAlertMessage, setActorSubmitAlertMessage] =
+    useState<string>("");
+  const [actorRemoveAlertOpen, setActorRemoveAlertOpen] =
+    useState<boolean>(false);
+  const [actorRemoveAlertMessage, setActorRemoveAlertMessage] =
+    useState<string>("");
 
   return (
     <AlertContext.Provider
-      value={{ actorSubmitAlertOpen, setActorSubmitAlertOpen }}
+      value={{
+        actorSubmitAlertOpen,
+        actorSubmitAlertMessage,
+        setActorSubmitAlertOpen,
+        setActorSubmitAlertMessage,
+        actorRemoveAlertOpen,
+        actorRemoveAlertMessage,
+        setActorRemoveAlertOpen,
+        setActorRemoveAlertMessage,
+      }}
     >
       {children}
     </AlertContext.Provider>
