056362365be41572f82de05fe99e9e16f2b022f1
StefMTP <sm@think-plus.gr>
2022-02-27 11:15:19 +0200

    scrap initiative marks for now

--- src/components/Round/RoundTimeline.tsx
+++ src/components/Round/RoundTimeline.tsx
@@ -1,73 +1,11 @@
 import { useContext } from "react";
 import { Timeline } from "@mui/lab";
 import { Slide, Typography } from "@mui/material";
-import { v4 as uuid } from "uuid";
 import RoundTimelineItem from "./Item/RoundTimelineItem";
 import { CombatActorsContext } from "../../contexts/CombatActorsContext";
-import { combatant } from "../../types";
-import { sortPlayerActors } from "../../helpers";
 
 const RoundTimeline = () => {
   const { combatActors } = useContext(CombatActorsContext);
-
-  const timelineItems: combatant[] = sortPlayerActors([
-    ...combatActors,
-    {
-      id: uuid(),
-      name: "Initiative Mark: 25",
-      initiative: 25,
-      alignment: "FOE",
-      type: "INITIATIVE_MARK",
-      color: {
-        primary: "info.main",
-        secondary: "info.light",
-      },
-    },
-    {
-      id: uuid(),
-      name: "Initiative Mark: 20",
-      initiative: 20,
-      alignment: "PARTY",
-      type: "INITIATIVE_MARK",
-      color: {
-        primary: "info.main",
-        secondary: "info.light",
-      },
-    },
-    {
-      id: uuid(),
-      name: "Initiative Mark: 15",
-      initiative: 15,
-      alignment: "FOE",
-      type: "INITIATIVE_MARK",
-      color: {
-        primary: "info.main",
-        secondary: "info.light",
-      },
-    },
-    {
-      id: uuid(),
-      name: "Initiative Mark: 10",
-      initiative: 10,
-      alignment: "PARTY",
-      type: "INITIATIVE_MARK",
-      color: {
-        primary: "info.main",
-        secondary: "info.light",
-      },
-    },
-    {
-      id: uuid(),
-      name: "Initiative Mark: 5",
-      initiative: 5,
-      alignment: "FOE",
-      type: "INITIATIVE_MARK",
-      color: {
-        primary: "info.main",
-        secondary: "info.light",
-      },
-    },
-  ]);
 
   return (
     <Slide in timeout={1000} direction="right">
@@ -77,7 +15,7 @@ const RoundTimeline = () => {
             You might wanna add some characters...
           </Typography>
         ) : (
-          timelineItems.map((combatActor, index, array) => (
+          combatActors.map((combatActor, index, array) => (
             <RoundTimelineItem
               key={combatActor.id}
               combatActor={combatActor}
